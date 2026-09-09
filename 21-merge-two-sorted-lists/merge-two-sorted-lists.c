04f971ba7e50def7ea88fbdab6ab47d2f4a78562
Naitik Srivastava <studynaitik04@gmail.com>
2026-09-10 00:56:42 +0530

    Time: 0 ms (100.00%) | Memory: 11.3 MB (95.57%) - LeetSync

---
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.c
@@ -0,0 +1,32 @@
+struct ListNode* mergeTwoLists(struct ListNode* list1,
+                               struct ListNode* list2) {
+
+    // Dummy node
+    struct ListNode dummy;
+    struct ListNode* current = &dummy;
+
+    dummy.next = NULL;
+
+    // Compare both lists
+    while (list1 != NULL && list2 != NULL) {
+
+        if (list1->val <= list2->val) {
+            current->next = list1;
+            list1 = list1->next;
+        }
+        else {
+            current->next = list2;
+            list2 = list2->next;
+        }
+
+        current = current->next;
+    }
+
+    // Attach remaining nodes
+    if (list1 != NULL)
+        current->next = list1;
+    else
+        current->next = list2;
+
+    return dummy.next;
+}
